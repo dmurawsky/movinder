3d8a42ef4b04c0c46a0d02bfe580aa553696ab90
Daniel Murawsky <d.r.murawsky@gmail.com>
2016-10-29 01:02:53 -0400

    fixed mispell of env

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.post('/favorites', function(req, res){
   }
 });
 
-app.listen(process.evn.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000");
 });
